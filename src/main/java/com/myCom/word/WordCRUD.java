0ab60015db54d8d117de74cecce5c30da54ed531
wnghks7787 <wnghks7787@naver.com>
2022-09-05 02:04:09 +0900

    Create WordCRUD.java

---
+++ src/main/java/com/myCom/word/WordCRUD.java
@@ -0,0 +1,23 @@
+package com.myCom.word;
+
+public class WordCRUD implements ICRUD {
+    @Override
+    public Object add() {
+        return null;
+    }
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    }
+}
