52d97a9eda8a0f08430b09f585fddc34c83d5821
wnghks7787 <wnghks7787@naver.com>
2022-09-06 02:30:57 +0900

    Update add() method

--- src/main/java/com/myCom/word/WordCRUD.java
+++ src/main/java/com/myCom/word/WordCRUD.java
@@ -1,9 +1,34 @@
 package com.myCom.word;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WordCRUD implements ICRUD {
+
+    ArrayList<Word> list;
+    Scanner s;
+
+    WordCRUD(Scanner s)
+    {
+        list = new ArrayList<>();
+        this.s = s;
+    }
     @Override
     public Object add() {
+        System.out.print("=> 난이도(1, 2, 3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
         return null;
+    }
+
+    public void addWord()
+    {
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다.");
     }
 
     @Override
