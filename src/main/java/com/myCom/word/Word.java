6716d152dba0f7f188cecf996f1e543b06eee7b7
wnghks7787 <wnghks7787@naver.com>
2022-09-06 02:43:04 +0900

    modify toString in Word.java

--- src/main/java/com/myCom/word/Word.java
+++ src/main/java/com/myCom/word/Word.java
@@ -7,6 +7,7 @@ public class Word {
     private String meaning;
 
     Word(){}
+
     Word(int id, int level, String word, String meaning)
     {
         this.id = id;
@@ -45,5 +46,18 @@ public class Word {
 
     public void setMeaning(String meaning) {
         this.meaning = meaning;
+    }
+
+    @Override
+    public String toString() {
+
+        String slevel = "";
+
+        for(int i = 0 ; i < level ; i++)
+            slevel += "*";
+        String str = String.format("%-3s", slevel)
+                + String.format("%15s", word) + " " + meaning;
+
+        return str;
     }
 }
